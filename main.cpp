6b3e4075a25aa6cf7580cf629e8e50eec7ea69ac
John Wonson <jmpwonson@gmail.com>
2020-12-22 10:22:45 +1100

    Create main.cpp
    Generate test_stream.txt.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#define ASYNC_IO_HEADER_ONLY
+
+#include "async_io.h"
+
+int main()
+{
+    async_io io_buffer{};
+    io_buffer.write("Test\n");
+    io_buffer.write(
+        std::make_unique<std::ofstream>("test_stream.txt", std::ios::app),
+        "Hello World\n");
+}
